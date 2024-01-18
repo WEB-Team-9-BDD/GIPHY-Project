0bb271308fce7b1ed935d566d3c3a9e86eca6486
borislavpachev <borislav.pachev@gmail.com>
2024-01-18 17:06:09 +0200

    favorites view edit

--- WEB Team Project/src/views/favorites-view.js
+++ WEB Team Project/src/views/favorites-view.js
@@ -1,4 +1,5 @@
-import { toMovieSimple } from './movie-views.js';
+// import { toMovieSimple } from './movie-views.js';
+import { renderFavoriteStatus } from './events/favorites-events.js';
 
 export const toFavoritesView = (movies) => `
 <div id="movies">
@@ -6,5 +7,16 @@ export const toFavoritesView = (movies) => `
   <div class="content">
     ${movies.map(toMovieSimple).join('\n') || '<p>Add some movies to favorites to see them here.</p>'}
   </div>
+</div>
+`;
+
+
+export const toMovieSimple = (movie) => `
+<div class="movie">
+  <h1>${movie.title}</h1>
+  <h2>${movie.year}</h2>
+  <img src="${movie.poster}"><br>
+  <button class="view-movie-btn" data-movie-id="${movie.id}">View details</button>
+  ${renderFavoriteStatus(movie.id)}
 </div>
 `;
