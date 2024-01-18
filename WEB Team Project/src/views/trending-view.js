adaa3b16e5852a96e92cb5a9d54a2b315aef921e
DDankov <danko5656@gmail.com>
2024-01-18 17:57:48 +0200

    toTrendingView Class

--- WEB Team Project/src/views/trending-view.js
+++ WEB Team Project/src/views/trending-view.js
@@ -1,21 +1,20 @@
 export const toTrendingView = (trendingGifs) => {
-    return `
+  return `
       <section class="trending">
         <h2>Trending</h2>
-        <ul>
+        <ul class="gif-grid">
           ${trendingGifs.map(toTrendingItemView).join('')}
         </ul>
       </section>
     `;
-    
-  };
-  
-  const toTrendingItemView = (trendingItem) => {
-    return `
+};
+
+const toTrendingItemView = (trendingItem) => {
+  return `
       <li>
         <a href='#/trending/${trendingItem.id}'>
           <img src="${trendingItem.images.fixed_width.url}"  alt="${trendingItem.title}" />
         </a>
       </li>
       `;
-  };
+};
