86ab47546b6c3590ed3193544048803b81b46720
borislavpachev <borislav.pachev@gmail.com>
2024-01-21 21:01:44 +0200

    search-view classes edit

--- WEB Team Project/src/views/search-view.js
+++ WEB Team Project/src/views/search-view.js
@@ -20,7 +20,7 @@
 export const toSearchView = (text, searchGifs) => {
   return `<div>
 <h2> Gifs with tag name: #${text}</h2>
-<div #gif-container>
+<div class="gif-container">
 ${searchGifs.map(toSingleGif).join('')}
 </div>
 </div>`;
@@ -29,6 +29,6 @@ ${searchGifs.map(toSingleGif).join('')}
 export const toSingleGif = (gifInfo) => `
 <div class="gif-item">
 <a href="#/search/${gifInfo.id}">
-<img class="search-item" src="${gifInfo.images.fixed_width.url} alt="${gifInfo.title}"
+<img class="gif-img-item" src="${gifInfo.images.fixed_width.url} alt="${gifInfo.title}"
 </div>
 `;
