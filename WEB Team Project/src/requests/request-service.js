c4bdd8410b295a56b32ce1988fe2a06bb6c51d3c
borislavpachev <borislav.pachev@gmail.com>
2024-01-21 19:44:58 +0200

    uploadGif POST Request

--- WEB Team Project/src/requests/request-service.js
+++ WEB Team Project/src/requests/request-service.js
@@ -1,11 +1,32 @@
-import { getSearchGifs, getTrendingURL } from '../common/constants.js';
+import { getSearchGifs, getTrendingURL, getUploadedURL } from '../common/constants.js';
+import { renderFailure, renderSuccess } from '../events/upload-events.js';
 
+export const uploadGif = async (username, url, tags, formData) => {
+  try {
+    const response = await fetch(getUploadedURL(username, url, tags),
+      {
+        method: 'POST',
+        body: formData,
+      });
+
+    const result = await response.json();
+
+    if (response.status === 200) {
+      renderSuccess(url);
+    } else if (response.status > 400 && response.status < 499) {
+      renderFailure(response.status);
+    };
+  } catch (error) {
+    console.error(error);
+    renderFailure();
+  }
+};
 
 export const loadSearchGifs = async (searchGif) => {
   const response = await fetch(getSearchGifs(searchGif));
   const result = await response.json();
   return result.data;
-}
+};
 
 export const loadTrendingGifs = async () => {
   const response = await fetch(getTrendingURL(30));
@@ -16,7 +37,7 @@ export const loadTrendingGifs = async () => {
 
 // export const loadCategories = () => {
 //   const categories = getCategories();
-  
+
 //   return categories;
 // };
 
@@ -35,7 +56,7 @@ export const loadTrendingGifs = async () => {
 // export const loadSingleMovie = (id) => {
 //   const movie = getMovieById(id);
 
-//   return movie;  
+//   return movie;
 // };
 
 // export const loadSearchMovies = (searchTerm = '') => {
