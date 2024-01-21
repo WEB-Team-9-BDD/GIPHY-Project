39d28ba7ca62674cda4aff9d413c6c7dbaf9b7ac
borislavpachev <borislav.pachev@gmail.com>
2024-01-21 19:41:37 +0200

    upload render success and failure cases

--- WEB Team Project/src/events/upload-events.js
+++ WEB Team Project/src/events/upload-events.js
@@ -8,3 +8,26 @@ export const renderLoadingView = () => {
     </div>
     `;
 };
+
+export const renderSuccess = (url) => {
+  q('.upload-container').innerHTML = `
+      <div class = "success-container">
+      <div class='success-upload'>
+      <h2>The GIF has been added successfully</h2>
+      </div>
+      <div id="success-image">
+      <img class="gif-img-item" src="${url}">
+      </div>
+      </div>`;
+};
+
+export const renderFailure = (status = '') => {
+  q('.upload-container').innerHTML = `
+      <div class = "fail-container">
+      <div class = "fail-message">
+      <h2>The GIF upload has failed!</h2>
+      <h3>${status} That's an error</h3>
+      <button class='try-again'> Please try again!</button>
+      </div>
+      </div>`;
+};
