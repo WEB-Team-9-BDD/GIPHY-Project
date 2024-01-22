a4360b6f804885bddbfaea63155dad64cda0af5a
borislavpachev <borislav.pachev@gmail.com>
2024-01-22 21:31:15 +0200

    uploaded gifs added

--- WEB Team Project/src/requests/request-service.js
+++ WEB Team Project/src/requests/request-service.js
@@ -1,6 +1,6 @@
-import { API_KEY } from '../common/constants.js';
+import { API_KEY, uploadedIdsEndpoint } from '../common/constants.js';
 import { getSearchGifs, getTrendingURL, getUploadedURL, getGifByID } from '../common/constants.js';
-import { renderFailure, renderSuccess } from '../events/upload-events.js';
+import { addUploadedGif, getUploadedIds, renderFailure, renderSuccess } from '../events/upload-events.js';
 
 export const uploadGif = async (url = '', sourceUrl = '', tags, formData = '') => {
   try {
@@ -11,19 +11,21 @@ export const uploadGif = async (url = '', sourceUrl = '', tags, formData = '') =
       });
 
     const result = await response.json();
-    // return result.data;
+
+    const id = await result.data.id;
 
     if (url && response.status === 200) {
+
       renderSuccess(url);
+      addUploadedGif(id);
 
     } else if (sourceUrl && response.status === 200) {
       renderSuccess(sourceUrl);
+      addUploadedGif(id);
 
     } else if (response.status >= 400 && response.status <= 500) {
       renderFailure(response.status);
     };
-
-
 
   } catch (error) {
     console.error(error);
@@ -80,32 +82,12 @@ export const loadDetails = async (id) => {
   return result.data;
 }
 
-// export const loadCategories = () => {
-//   const categories = getCategories();
+export const loadUploadedGifs = async () => {
+  const idsList = getUploadedIds();
 
-//   return categories;
-// };
+  const response = await fetch(uploadedIdsEndpoint(idsList));
+  const result = await response.json();
 
-// export const loadCategory = (id = null) => {
-//   const category = getCategory(id);
+  return result.data;
+};
 
-//   return category;
-// }
-
-// export const loadMovies = (categoryId = null) => {
-//   const movies = getMoviesGeneralInfo(categoryId);
-
-//   return movies;
-// };
-
-// export const loadSingleMovie = (id) => {
-//   const movie = getMovieById(id);
-
-//   return movie;
-// };
-
-// export const loadSearchMovies = (searchTerm = '') => {
-//   const movies = searchMovies(searchTerm);
-
-//   return movies;
-// };
