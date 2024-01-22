c3c5d377d66b1ab645d897bf23de4d87b1a022ae
DDankov <danko5656@gmail.com>
2024-01-22 15:27:40 +0200

    favorites-view fix

--- WEB Team Project/src/views/favorites-view.js
+++ WEB Team Project/src/views/favorites-view.js
@@ -1,23 +1,50 @@
 // import { toMovieSimple } from './movie-views.js';
-// import { renderFavoriteStatus } from './events/favorites-events.js';
+import { renderFavoriteStatus } from '../events/favorites-events.js';
+import { API_KEY } from '../common/constants.js';
+import { fetchFavorites, loadRandomGif } from '../requests/request-service.js';
+import { fetchGifById } from '../requests/request-service.js';
+import { getFavorites } from '../data/favorites.js';
 
-export const toFavoritesView = (movies) => `
-<div id="movies">
-  <h1>Favorite GIFs:</h1>
-  <div class="content">
-  </div>
-</div>
-`;
+export const toFavoritesView = async (favGifsId) => {
+  const result = await loadRandomGif();
 
-// after content class
-//     ${movies.map(toMovieSimple).join('\n') || '<p>Add some movies to favorites to see them here.</p>'}
+  if (favGifsId.length === 0) {
+    return `<p>Add some GIFs to favorites to see them here.</p>
+    <li class="randomGif">
+    <img src="${result.images.fixed_width.url}" width="150" height="150" alt="${result.title}"
+    <div class="gif-details">
+      <p>${result.title}</p>
+    </div>
+    </li>`;
+  }
 
-// export const toMovieSimple = (movie) => `
-// <div class="movie">
-//   <h1>${movie.title}</h1>
-//   <h2>${movie.year}</h2>
-//   <img src="${movie.poster}"><br>
-//   <button class="view-movie-btn" data-movie-id="${movie.id}">View details</button>
-//   ${renderFavoriteStatus(movie.id)}
+  //const favGifs = await Promise.all(favGifsId.map(id => fetchGifById(id)));
+  const favGifsIds = getFavorites().join(', ');
+  console.log(favGifsIds);
+  const favGifs = await fetchFavorites(favGifsIds);
+
+  return `
+  <div id="favorites">
+    <h1>Favorite GIFs:</h1>
+    <ul class="content">
+      ${favGifs.map(gif => gif ? `
+        <li class="gif-item">
+          <img src="${gif.images.fixed_height.url}" alt="${gif.title}">
+          ${renderFavoriteStatus(gif.id)}
+          <p>${gif.title}</p>
+        </li>
+        ` : '<p>GIF cannot load</p>').join('')}
+    </ul>
+  </div>`;;
+};
+
+
+// const toGifSimple = (gif) => `
+// <div class="gif">
+//   <h1>${gif.title}</h1>
+//   <h2>${gif.year}</h2>
+//   <img src="${gif.poster}"><br>
+//   <button class="" gif-id="${gif.id}">View details</button>
+//   ${renderFavoriteStatus(gif.id)}
 // </div>
 // `;
