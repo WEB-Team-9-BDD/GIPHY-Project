393a9e8880e98361b2dc70cd6030e705b625d4cd
DDankov <danko5656@gmail.com>
2024-01-22 15:25:50 +0200

    favorites-event fix

--- WEB Team Project/src/events/favorites-events.js
+++ WEB Team Project/src/events/favorites-events.js
@@ -2,25 +2,25 @@ import { EMPTY_HEART, FULL_HEART } from '../common/constants.js';
 import { addFavorite, getFavorites, removeFavorite } from '../data/favorites.js';
 import { q } from './helpers.js';
 
-export const toggleFavoriteStatus = (movieId) => {
+export const toggleFavoriteStatus = (gifId) => {
   const favorites = getFavorites();
-  const heartSpan = q(`span[data-movie-id="${movieId}"]`);
-  
-  if (favorites.includes(movieId)) {
-    removeFavorite(movieId);
-    heartSpan.classList.remove('active')
+  const heartSpan = q(`button[data-gif-id="${gifId}"]`);
+
+  if (favorites.includes(gifId)) {
+    removeFavorite(gifId);
+    heartSpan.classList.remove('active');
     heartSpan.innerHTML = EMPTY_HEART;
   } else {
-    addFavorite(movieId);
+    addFavorite(gifId);
     heartSpan.classList.add('active');
     heartSpan.innerHTML = FULL_HEART;
   }
 };
 
-export const renderFavoriteStatus = (movieId) => {
+export const renderFavoriteStatus = (gifId) => {
   const favorites = getFavorites();
 
-  return favorites.includes(movieId)
-    ? `<span class="favorite active" data-movie-id="${movieId}">${FULL_HEART}</span>`
-    : `<span class="favorite" data-movie-id="${movieId}">${EMPTY_HEART}</span>`;
+  return favorites.includes(gifId)
+    ? `<button class="add-favorite-btn active" data-gif-id="${gifId}">${FULL_HEART}</button>`
+    : `<button class="add-favorite-btn" data-gif-id="${gifId}">${EMPTY_HEART}</button>`;
 };
