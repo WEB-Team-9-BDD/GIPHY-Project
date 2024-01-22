2a52f30fe578c9183d08460c55cfcce096a5b6e5
DDankov <danko5656@gmail.com>
2024-01-22 15:27:19 +0200

    request-service fix

--- WEB Team Project/src/requests/request-service.js
+++ WEB Team Project/src/requests/request-service.js
@@ -1,11 +1,13 @@
-import { getSearchGifs, getTrendingURL } from '../common/constants.js';
+import { getRandomGifs, getSearchGifs, getTrendingURL } from '../common/constants.js';
+import { API_KEY } from '../common/constants.js';
 
 
 export const loadSearchGifs = async (searchGif) => {
   const response = await fetch(getSearchGifs(searchGif));
   const result = await response.json();
+
   return result.data;
-}
+};
 
 export const loadTrendingGifs = async () => {
   const response = await fetch(getTrendingURL(30));
@@ -14,9 +16,45 @@ export const loadTrendingGifs = async () => {
   return result.data;
 };
 
+export const loadRandomGif = async () => {
+  try {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`);
+    const result = await response.json();
+
+    return result.data;
+  } catch (error) {
+    console.error('Error fetching random GIF', error);
+  }
+};
+
+export const fetchGifById = async (gifId) => {
+  try {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/${gifId}?api_key=${API_KEY}`);
+    const data = await response.json();
+    console.log(`Here we catch GIFId favs: ${data}`);
+    return data.data;
+  } catch (error) {
+    console.error('Error fetching GIF by ID:', error);
+    return null;
+  }
+};
+
+export const fetchFavorites = async (gifIds) => {
+  try {
+    const response = await fetch(`https://api.giphy.com/v1/gifs?api_key=${API_KEY}&ids=${gifIds}`);
+    const data = await response.json();
+    console.log(`Here we catch GIFId favs: ${data}`);
+    return data.data;
+  } catch (error) {
+    console.error('Error fetching GIF by ID:', error);
+    return null;
+  }
+}
+
+
 // export const loadCategories = () => {
 //   const categories = getCategories();
-  
+
 //   return categories;
 // };
 
@@ -35,7 +73,7 @@ export const loadTrendingGifs = async () => {
 // export const loadSingleMovie = (id) => {
 //   const movie = getMovieById(id);
 
-//   return movie;  
+//   return movie;
 // };
 
 // export const loadSearchMovies = (searchTerm = '') => {
