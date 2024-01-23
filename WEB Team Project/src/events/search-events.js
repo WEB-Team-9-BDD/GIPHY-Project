23fc1ba2c27fa70c5c310a12cce118c7d6db9eda
borislavpachev <borislav.pachev@gmail.com>
2024-01-23 21:26:07 +0200

    js docs added

--- WEB Team Project/src/events/search-events.js
+++ WEB Team Project/src/events/search-events.js
@@ -3,8 +3,15 @@ import { loadSearchGifs } from '../requests/request-service.js';
 import { toSearchView } from '../views/search-view.js';
 import { q } from './helpers.js';
 
-export const renderSearchGifs = async (searchGif) => {
-  const gifs = await loadSearchGifs(searchGif);
 
-  q(CONTAINER_SELECTOR).innerHTML = toSearchView(searchGif, gifs);
+/**
+ * A function that uses loadSearchGifs for asynchronous loading the gifs by the searched
+ * parameter
+ * @param {string} searchTerm - The keyword with whom you are finding the gif
+ * The function is adding HTML on the page with resulting GIFs.
+ */
+export const renderSearchGifs = async (searchTerm) => {
+  const gifs = await loadSearchGifs(searchTerm);
+
+  q(CONTAINER_SELECTOR).innerHTML = toSearchView(searchTerm, gifs);
 };
